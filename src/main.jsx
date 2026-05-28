9be45f2e45ff542bbfd25162937866d38bd9f729
pdeljoo <payamdeljoo@gmail.com>
2026-05-28 13:06:08 -0700

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import MillworkPDFEditor from "./MillworkPDFEditor.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <MillworkPDFEditor />
-  </React.StrictMode>
-);
